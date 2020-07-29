84de49c4f283b2764d8250c219be7885904ab0f8
Tiauna Paul <tiaunaj.tsg@gmail.com>
2020-07-29 21:26:49 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,6 +1,14 @@
 require_relative './fancy_dance.rb'
 
 class Kid 
+  include Dance 
+  
+  attr_accessor:name 
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
  extend FancyDance::ClassMethods
  include FancyDance::InstanceMethods
 end
